7682091e74ce8c83bc2bdacdedaaad19cbc48d17
T0ADY_ <105169067+T0ADY-ytb@users.noreply.github.com>
2025-08-13 05:09:41 +0200

    Add 3 new jokers

---
+++ jokers/steel_bar.lua
@@ -0,0 +1,37 @@
+SMODS.Joker{ --Steel Bar
+    key = "steel_bar",
+    config = {
+        extra = {
+            steelcardsindeck = 0
+        }
+    },
+    loc_txt = {
+        ['name'] = 'Steel Bar',
+        ['text'] = {
+            [1] = 'Gives {X:chips,C:white}X0.1{} Chips for each {C:attention}Steel Card{} in your {C:attention}full deck{}'
+        },
+        ['unlock'] = {
+            [1] = 'Unlocked by default.'
+        }
+    },
+    pos = {
+        x = 1,
+        y = 0
+    },
+    cost = 5,
+    rarity = 2,
+    blueprint_compat = true,
+    eternal_compat = true,
+    perishable_compat = true,
+    unlocked = true,
+    discovered = false,
+    atlas = 'CustomJokers',
+
+    calculate = function(self, card, context)
+        if context.cardarea == G.jokers and context.joker_main  then
+                return {
+                    x_chips = ((function() local count = 0; for _, card in ipairs(G.playing_cards or {}) do if SMODS.has_enhancement(card, 'm_steel') then count = count + 1 end end; return count end)()) * 0.1
+                }
+        end
+    end
+}
