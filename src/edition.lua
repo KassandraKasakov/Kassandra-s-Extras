3ddc1e6b9e154129326a7af2f2c2c7dbf81d5760
T0ADY_ <105169067+T0ADY-ytb@users.noreply.github.com>
2025-09-03 15:09:03 +0200

    add Cosmic effect

--- src/edition.lua
+++ src/edition.lua
@@ -1,25 +1,54 @@
 SMODS.Edition {
-    key = 'cosmic',
+    key = 'astral',
+    loc_txt = {
+        name = 'Cosmic',
+        text = {
+            '{C:purple}+#1#{} Score',
+        },
+        label = 'Cosmic',
+    },
     shader = 'astral',
+    weight = 16,
     in_shop = true,
-    weight = 0.25,
     extra_cost = 4,
+
     apply_to_float = true,
     badge_colour = HEX('263F61'),
     disable_shadow = false,
     disable_base_shader = false,
-    loc_txt = {
-        name = 'Cosmic',
-        label = 'Cosmic',
-        text = {
-        [1] = 'A {C:blue}custom{} edition with {C:red}unique{} effects.'
-    }
-    },
     unlocked = true,
     discovered = true,
     no_collection = false,
-    get_weight = function(self)
-        return G.GAME.edition_rate * self.weight
-    end,
-  
+
+	config = {
+		score = 800,
+        active = false,
+    },
+
+    loc_vars = function(self, info_queue, center)
+		return {
+			vars = {self.config.score, self.config.active}
+		}
+	end,
+
+    calculate = function(self, card, context)
+        if context.post_joker and not context.repetition then
+            self.config.active = true
+        end
+        if context.after and self.config.active == true then
+			G.E_MANAGER:add_event(Event({trigger = 'after', delay = 0.4,
+				func = function() 
+					G.GAME.chips = (to_big(G.GAME.chips))+(to_big(self.config.score))
+					G.HUD:get_UIE_by_ID('chip_UI_count'):juice_up(0.3, 0.3)
+					play_sound('gong')
+                    self.config.active = false
+					return true
+				end,
+			}))
+            return {
+				message = "X" .. tostring(self.config.score),
+				colour = G.C.PURPLE
+			}
+        end
+	end
 }
